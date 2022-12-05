3e524f250445a2e5f3d5924b8549e1d9f7189c6f
JG <gunn.gsd@gmail.com>
2022-12-05 04:09:23 -0500

    more edits

---
+++ docker-mysql-8/create-table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE 'employee-schema'.'employee' ( 'emp_id' INT(11) NOT NULL, 'first_name' VARCHAR(45) NULL, 'last_name' VARCHAR(45) NULL, 'email_id' VARCHAR(45) NULL, PRIMARY KEY ('emp_id'));
